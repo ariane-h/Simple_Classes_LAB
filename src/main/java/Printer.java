778a87cd318f3f9af0f5a5f6a56790b23eaf0489
Ariane Holford <aribomop@gmail.com>
2020-04-27 15:46:43 +0100

    add print function

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Printer {
+    private int paper;
+
+    public Printer(int paper){
+        this.paper = paper;
+    }
+
+    public int getPaper(){
+        return paper;
+    }
+
+    public int print(int pages, int copies) {
+      return paper -= (pages * copies);
+    };
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer(100);
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(100, printer.getPaper());
+    }
+
+    @Test
+    public void canPrint(){
+        printer.print(2, 5);
+        assertEquals(90, printer.getPaper());
+    }
 }
